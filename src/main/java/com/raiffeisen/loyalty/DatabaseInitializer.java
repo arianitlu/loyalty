005fc5ab8bdc696cff7c9862a65f1e5ecc3080fb
Arianit <arianit.lu@hotmail.com>
2024-05-08 13:20:45 +0200

    add transaction details to loyalty points

--- src/main/java/com/raiffeisen/loyalty/DatabaseInitializer.java
+++ src/main/java/com/raiffeisen/loyalty/DatabaseInitializer.java
@@ -3,6 +3,8 @@ package com.raiffeisen.loyalty;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
 
@@ -14,8 +16,8 @@ public class DatabaseInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        LoyaltyPoints loyaltyPoints1 = new LoyaltyPoints(1L, 100, PointStatus.AVAILABLE);
-        LoyaltyPoints loyaltyPoints2 = new LoyaltyPoints(2L, 200, PointStatus.PENDING);
+        LoyaltyPoints loyaltyPoints1 = new LoyaltyPoints(1L, 100, 4500, LocalDateTime.of(2022, 6, 15, 10, 30), 4500, PointStatus.PENDING);
+        LoyaltyPoints loyaltyPoints2 = new LoyaltyPoints(2L, 101,7800, LocalDateTime.of(2022, 6, 15, 10, 30),10900, PointStatus.PENDING);
         loyaltyPointsRepository.save(loyaltyPoints1);
         loyaltyPointsRepository.save(loyaltyPoints2);
     }
--- src/main/java/com/raiffeisen/loyalty/LoyaltyPoints.java
+++ src/main/java/com/raiffeisen/loyalty/LoyaltyPoints.java
@@ -5,20 +5,29 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class LoyaltyPoints {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long loylatyPointsId;
+
+    private long transactionId;
+    private int transactionAmount;
+    private LocalDateTime transactionDate;
     private int points;
     private PointStatus status;
 
     public LoyaltyPoints() {
     }
 
-    public LoyaltyPoints(long loylatyPointsId, int points, PointStatus status) {
+    public LoyaltyPoints(long loylatyPointsId, long transactionId, int transactionAmount, LocalDateTime transactionDate, int points,  PointStatus status) {
         this.loylatyPointsId = loylatyPointsId;
+        this.transactionId = transactionId;
+        this.transactionAmount = transactionAmount;
+        this.transactionDate = transactionDate;
         this.points = points;
         this.status = status;
     }
@@ -45,5 +54,29 @@ public class LoyaltyPoints {
 
     public void setStatus(PointStatus status) {
         this.status = status;
+    }
+
+    public int getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(int transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime transactionDate) {
+        this.transactionDate = transactionDate;
     }
 }
